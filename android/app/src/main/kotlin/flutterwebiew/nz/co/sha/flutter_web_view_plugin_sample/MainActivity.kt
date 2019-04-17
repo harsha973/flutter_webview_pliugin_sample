0fc9c49250c1b18287fb9bf1c8158c49b14cf60f
Sree <sree@Srees-MacBook-Pro.local>
2019-04-18 09:01:51 +1200

    Req #Open Google

---
+++ android/app/src/main/kotlin/flutterwebiew/nz/co/sha/flutter_web_view_plugin_sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package flutterwebiew.nz.co.sha.flutter_web_view_plugin_sample
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
